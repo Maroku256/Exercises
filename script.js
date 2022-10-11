842dceed65b3892dfa29a9e60c3d208b937387a2
Maroku256 <81808337+Maroku256@users.noreply.github.com>
2022-10-11 10:46:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ $("html").click(function (event) {
 */
 
 //Задание 2
+/*
 var limit = 10;
 var time = 50;
 var direction = 'вправо';
@@ -124,6 +125,7 @@ var Square = function () {
 };
 
 var interval = setInterval(Square, time);
+*/
 //Задание 3
 /*
 $("h1").click(function (){
@@ -132,6 +134,7 @@ $("h1").click(function (){
 */
 
 //Задание 4
+/*
 $("h1").click(function (){
     time -= 1
     interval = setInterval(Square, time)
@@ -141,6 +144,143 @@ $("h1").click(function (){
         $("h1").text("Вы победили!"); 
     };
 });
+*/
+
+
+
+
+/*
+var Car = function(x, y){
+    this.x = x;
+    this.y = y;
+    this.draw();//вызов draw
+};
+
+Car.prototype.draw = function(){
+    var carHtml = '<img src = "/car.webp">';
+
+    this.carElement = $(carHtml);
+
+    this.carElement.css({
+        position: "absolute",
+        left: this.x,
+        top: this.y
+    });
+
+    $("body").append(this.carElement);
+}
+
+Car.prototype.moveRight = function(speed){
+    this.x += speed;
+
+    this.carElement.css({
+        left: this.x,
+        top: this.y
+    });
+};
+
+Car.prototype.moveLeft = function(speed){
+    this.x -= speed;
+
+    this.carElement.css({
+        left: this.x,
+        top: this.y
+    });
+};
+
+Car.prototype.moveUp = function(speed){
+    this.y -= speed;
+
+    this.carElement.css({
+        left: this.x,
+        top: this.y
+    });
+};
+
+Car.prototype.moveDown = function(speed){
+    this.y += speed;
+
+    this.carElement.css({
+        left: this.x,
+        top: this.y
+    });
+};
+
+var tesla = new Car(10, 20);
+var nissan = new Car(10, 200);
+
+var speed;//свойство speed
+//гонки)
+setInterval(function(){tesla.moveRight(speed = Math.floor(Math.random() * 5))}, 30);
+setInterval(function(){nissan.moveRight(speed = Math.floor(Math.random() * 5))}, 30);
+
+*/
+
+//Задание 1
+/*
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.lineWidth = 2;
+ctx.strokeStyle = "Black"
+
+
+var snowman = function(x, y){
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, Math.PI * 2, false);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(x - 10, y - 3, 3, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(x + 10, y - 3, 3, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(x, y + 50, 30, 0, Math.PI * 2, false);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(x, y + 120, 40, 0, Math.PI * 2, false);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.fillStyle = "Orange"
+    ctx.strokeStyle = "Orange"
+    ctx.arc(x, y + 3, 2, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.stroke();
+
+}
+snowman(50, 50);
+*/
+
+var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    var drawPoints = function (points) {
+      ctx.beginPath();
+      ctx.moveTo(points[0][0], points[0][1]);
+      for (var i = 1; i < points.length; i++) {
+        ctx.lineTo(points[i][0], points[i][1]);
+      }
+      ctx.stroke();
+    };
+
+    var mysteryPoints = [[50, 50], [50, 100], [25, 120], [100, 50], [70, 90], [100, 90], [70, 120]];
+    drawPoints(mysteryPoints);
+
+
+
+
+
+
+
+
+
 
 
 
